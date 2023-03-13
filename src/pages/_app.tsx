834fe328a6ccdaac5c21c1b45203a4fd947680c1
Jacek Pietrzak <wiewiex@gmail.com>
2023-03-13 12:14:50 +0100

    global styles refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,12 @@
 import type { AppProps } from 'next/app';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import theme from '@/utils/theme';
-import '../utils/importFonts.css';
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`;
+import './_app.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
